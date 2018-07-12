95a7a9dc0195aaa9a4133329c75239bca08dcca5
Pablo J Colon <pabloj.colon@computerpropr.com>
2018-07-12 10:13:33 -0400

    Added the code to filter the STAFFMASTER files codes with the codes in the JSON file.

--- cleanStaff.py
+++ cleanStaff.py
@@ -6,7 +6,6 @@ def clean_staff():
 
 
 
-
     # Inicializar listado en jsonxx
     with open('dealerCodes.json', 'r') as f:
         dealer_dict = json.load(f)
@@ -15,6 +14,7 @@ def clean_staff():
     # Inicializar excel workook
     wb = Workbook()
     ws = wb.active
+    record = ""
     count = 0
     row_line = 2
     stafffile = input("Escriba el nombre del archivo: ")
@@ -27,20 +27,20 @@ def clean_staff():
         has_term_date = (line[8][0:1]).isdigit()
         has_t = line[9].startswith("T")
         have_blank_fields = (line[5] + " " + line[6] + " " + line[7] + " " + line[8] + " " + line[9]).strip()
+        txt_line = line[0]
 
-        if not has_t and not has_term_date and have_blank_fields != "":
+        for emp_code_json in dealer_dict['dealer_codes']:
+            emp_code_json = str(emp_code_json['code'])
 
-            row_line = row_line + 1
+            if txt_line == emp_code_json:
+                record = line[0]
 
-            for empcode in dealer_dict['dealer_codes']:
-                empcode = str(empcode['code'])
+        if not has_t and not has_term_date and have_blank_fields != "" and record != line[0]:
 
-                if empcode != line[0]:
+            print(line)
+            count = count + 1
 
-                    continue
-                else:
-                    print(line)
-                    count = count + 1
+            # row_line = row_line + 1
             # send data to Excel
             # a2 = ws.cell(row=row_line, column=1)
             # a2.value = line[0]
@@ -58,7 +58,6 @@ def clean_staff():
             # g2.value = line[6]
             # h2 = ws.cell(row=row_line, column=8)
             # h2.value = line[7]
-
 
     print("Cantidad de records: " + str(count))
     wb.save(stafffile + ".xlsx")
