b0bd9b0f48a5c59879f111685361e2313cb2c8dd
Pablo J Colon <pabloj.colon@computerpropr.com>
2018-07-12 17:44:23 -0400

    Activated the code to tranfer data to Excel

--- cleanStaff.py
+++ cleanStaff.py
@@ -16,11 +16,29 @@ def clean_staff():
     ws = wb.active
     record = ""
     count = 0
-    row_line = 2
+    row_line = 1
     stafffile = input("Escriba el nombre del archivo: ")
     # Encargado de leer el archivo de staffmaster
     staff_namefile = './input/' + stafffile + '.txt'
     staff_textfile = open(staff_namefile, 'r')
+
+    # Excel Header
+    column_a = ws.cell(row=1, column=1)
+    column_a.value = "Dealer Code"
+    column_b = ws.cell(row=1, column=2)
+    column_b.value = "Spin"
+    column_c = ws.cell(row=1, column=3)
+    column_c.value = "Name"
+    column_d = ws.cell(row=1, column=4)
+    column_d.value = "Middle Name"
+    column_e = ws.cell(row=1, column=5)
+    column_e.value = "Last Name"
+    column_f = ws.cell(row=1, column=6)
+    column_f.value = "Job Code"
+    column_g = ws.cell(row=1, column=7)
+    column_g.value = "Department"
+    column_h = ws.cell(row=1, column=8)
+    column_h.value = "Hired Date"
 
     for line in staff_textfile:
         line = line.split("|")
@@ -37,27 +55,27 @@ def clean_staff():
 
         if not has_t and not has_term_date and have_blank_fields != "" and record != line[0]:
 
-            print(line)
+            #print(line)
             count = count + 1
 
-            # row_line = row_line + 1
-            # send data to Excel
-            # a2 = ws.cell(row=row_line, column=1)
-            # a2.value = line[0]
-            # b2 = ws.cell(row=row_line, column=2)
-            # b2.value = line[1]
-            # c2 = ws.cell(row=row_line, column=3)
-            # c2.value = line[2]
-            # d2 = ws.cell(row=row_line, column=4)
-            # d2.value = line[3]
-            # e2 = ws.cell(row=row_line, column=5)
-            # e2.value = line[4]
-            # f2 = ws.cell(row=row_line, column=6)
-            # f2.value = line[5]
-            # g2 = ws.cell(row=row_line, column=7)
-            # g2.value = line[6]
-            # h2 = ws.cell(row=row_line, column=8)
-            # h2.value = line[7]
+            row_line = row_line + 1
+            #send data to Excel
+            a2 = ws.cell(row=row_line, column=1)
+            a2.value = line[0]
+            b2 = ws.cell(row=row_line, column=2)
+            b2.value = line[1]
+            c2 = ws.cell(row=row_line, column=3)
+            c2.value = line[2]
+            d2 = ws.cell(row=row_line, column=4)
+            d2.value = line[3]
+            e2 = ws.cell(row=row_line, column=5)
+            e2.value = line[4]
+            f2 = ws.cell(row=row_line, column=6)
+            f2.value = line[5]
+            g2 = ws.cell(row=row_line, column=7)
+            g2.value = line[6]
+            h2 = ws.cell(row=row_line, column=8)
+            h2.value = line[7]
 
     print("Cantidad de records: " + str(count))
     wb.save(stafffile + ".xlsx")
